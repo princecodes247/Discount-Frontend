ea25e46e382435f88b03d14118f13399ae745bd6
PrinceCodes <princecodes247@gmail.com>
2021-12-26 20:16:21 +0100

    feat: create Icons Grid Component

---
+++ components/IconGrid/index.js
@@ -0,0 +1,19 @@
+
+ // import styles from './ItemCard.module.css'
+
+// import { useState, useEffect } from "react"
+  const IconGrid = (props) => {
+    return <ul className="deals">
+            <li className="deal">
+              <div className="deal_img"></div>
+              <p>Top Deals</p>
+            </li>
+            <li className="deal">
+              <div className="deal_img"></div>
+              <p>Top Deals</p>
+            </li>
+          </ul>
+
+  }
+
+  export default IconGrid;
